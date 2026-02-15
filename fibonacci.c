63aa28ec872ce5c2d9433b66856317af5418b7aa
Haris Abdullah <7harisabdullah@gmail.com>
2026-02-15 16:15:33 +0500

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -9,7 +9,7 @@ size_t fibonacci(size_t n) {
     for (size_t i = 0; i < n / 2; i++) { //we already have one batch cooked initially, so we go i < n / 2 (instead of i <= n / 2).
 
         array[0] += array[1]; //array[0] = f[k], and array[0] turns to f[k] + f[k + 1] i.e f[k + 2]
-        array[1] += array[0]; //array[1] = f[k + 1] and array[1] turns to f[k + 1] + f[k + 2] i.e. f[k + 3] //note that array[0] turned tp f[k + 2] just a while before.
+        array[1] += array[0]; //array[1] = f[k + 1] and array[1] turns to f[k + 1] + f[k + 2] i.e. f[k + 3] //note that array[0] turned tp f[k + 2] just a while ago.
 
         //this is clearly batch updation + sliding window technique.
     }
@@ -17,7 +17,7 @@ size_t fibonacci(size_t n) {
     //after m iterations, array = {f[2 * m], f[2 * m + 1]}
 
     //if n is even, we perform n / 2 iterations. //array = {f[n] + f[n + 1]}
-    //if k is odd, we perform (n - 1) / 2 iterations. //array = {f[n - 1], f[n]}
+    //if n is odd, we perform (n - 1) / 2 iterations. //array = {f[n - 1], f[n]}
 
     return array[n % 2]; //if n is even, f[n] is at 0 index and if n is odd, f[n] is at 1 index. //simply (n % 2) index.
 }
